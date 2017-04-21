ab982278de5c2b1504547e5177833ed257736b3b
tdrier1 <tdrier1@gmail.com>
2017-04-21 15:48:50 -0400

    Finished TDD

---
+++ Lab_16TDD/Lab_16Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_16TDD
+{
+    class Lab_16Test
+    {
+
+        public static bool PrimeTest(int num1)
+        {
+            if (num1 == 0 || num1 == 1)
+            {
+                Console.WriteLine(num1 + " is not prime number");
+                return false;
+            }
+            else
+            {
+                for (int a = 2; a <= num1 / 2; a++)
+                {
+                    if (num1 % a == 0)
+                    {
+                        Console.WriteLine(num1 + " is not prime number");
+                        return false;
+                    }
+
+                }
+                Console.WriteLine(num1 + " is a prime number");
+                return true;
+
+            }
+
+            }
+        }
+    }       
+
+
+
---
+++ Lab_16TDD/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab_16TDD
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodIsNotPrime1()
+        {
+            bool Expected = false;
+
+            Assert.AreEqual(Expected, Lab_16Test.PrimeTest(1));
+        }
+
+        [TestMethod]
+        public void TestMethodIsNotPrime2()
+        {
+            bool Expected = true;
+
+            Assert.AreEqual(Expected, Lab_16Test.PrimeTest(7));
+        }
+
+        [TestMethod]
+        public void TestMethodIsAPrime3()
+        {
+            bool Expected = false;
+
+            Assert.AreEqual(Expected, Lab_16Test.PrimeTest(8));
+        }
+    }
+    }
+
